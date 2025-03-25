269c1e9c5d79c4191c94b0644c8b709b096d3e8a
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-03-26 00:25:04 +0100

    remove diffs endpoint

--- server/main.go
+++ server/main.go
@@ -10,29 +10,8 @@ import (
 func main() {
 
 	serveMux := http.NewServeMux()
-	// serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("Hello WiDiff"))
-	// })
 
 	serveMux.HandleFunc("/diff", func(w http.ResponseWriter, r *http.Request) {
-		// diff, err := wikiapi.GetDiff(wikiapi.TestUrl)
-		diff, err := wikiapi.GetDiff(wikiapi.TestRequest.URL())
-		if err != nil {
-			log.Printf("could not retrieve diff: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		parsed, err := wikiapi.ParseDiffText(diff.Compare)
-		if err != nil {
-			log.Printf("could not parse diff: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Write([]byte(parsed))
-	})
-
-	serveMux.HandleFunc("/diffs", func(w http.ResponseWriter, r *http.Request) {
 		recents, err := wikiapi.GetRecentChanges()
 		if err != nil {
 			log.Printf("could not retrieve recents: %s", err)
