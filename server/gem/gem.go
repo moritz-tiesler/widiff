d575f2b0a45d111ae008c4fdc7773f9c55bb0c02
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-03-31 20:23:04 +0200

    gemini client

---
+++ server/gem/gem.go
@@ -0,0 +1,65 @@
+package gem
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+var systemInstruction string = `
+you judge the wikipedia entry diff like you were a snarky senior dev reviewing a PR.
+You will receive the comment followed by a string in unified diff format.
+Complain if the diff is too long. As you would if a PR was too long.
+`
+
+type Gem struct {
+	client *genai.Client
+	model  *genai.GenerativeModel
+}
+
+func New() (*Gem, error) {
+	key, ok := os.LookupEnv("GEMINI_API_KEY")
+	if !ok {
+		log.Println("GEMINI_API_KEY not set")
+		return nil, fmt.Errorf("GEMINI_API_KEY not set")
+	}
+	os.Getenv("GEMINI_API_KEY")
+	ctx := context.Background()
+	client, err := genai.NewClient(
+		ctx,
+		option.WithAPIKey(key),
+	)
+	model := client.GenerativeModel("gemini-2.0-flash")
+	model.SetMaxOutputTokens(100)
+	model.SystemInstruction = genai.NewUserContent(
+		genai.Text(systemInstruction),
+	)
+	model.ResponseMIMEType = "application/json"
+
+	return &Gem{client, model}, err
+}
+
+func (g *Gem) Generate(prompt string) (string, error) {
+	resp, err := g.model.GenerateContent(context.Background(), genai.Text("What is the average size of a swallow?"))
+	if err != nil {
+		return "", err
+	}
+	return printResponse(resp), nil
+}
+
+func printResponse(resp *genai.GenerateContentResponse) string {
+	var b bytes.Buffer
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Fprint(&b, part)
+			}
+		}
+	}
+	return b.String()
+}
